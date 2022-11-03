9c2eed66bd56453ae163b19f0a5a53e911805e9d
Zaki <zakiamanullah215@gmail.com>
2022-11-03 20:44:30 +0700

    Menambah fungsi untuk mendapatkan matriks kovarian

--- averageface.py
+++ averageface.py
@@ -4,17 +4,17 @@ import cv2
 def avgFace():
     init = np.zeros([256, 256], dtype=int);
     for i in range(1,99):
-        path = "../ALGEO-2/data/gray/CR" + str(i) + ".png";
+        path = "../ALGEO02-21109/data/gray/CR" + str(i) + ".png";
         img = cv2.imread(path, 0);
         init = np.add(init, img);
-        print(init);
     
     init = np.multiply(1/99, init);
     init = init.astype(np.uint8);
     return init;
 
 # img = cv2.imread("../ALGEO-2/data/gray/CR1.png", 0);
-im = avgFace();
-cv2.imshow("sewey", im);
-cv2.waitKey(0);
-cv2.destroyAllWindows();
+if __name__ == "__main__":
+    im = avgFace();
+    cv2.imshow("sewey", im);
+    cv2.waitKey(0);
+    cv2.destroyAllWindows();
---
+++ getcovariant.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+from averageface import avgFace
+
+def getCov(avgimage):
+    res = np.empty(shape=(65536, 0))
+
+    for i in range(1, 99):
+        path = "../ALGEO02-21109/data/gray/CR" + str(i) + ".png";
+        im = cv2.imread(path, 0)
+        temp = np.subtract(im, avgimage)
+        temp = temp.reshape([65536, 1])
+        res = np.append(res, temp, axis=1)
+
+    transposed = np.transpose(res)
+    cov = np.matmul(transposed, res)
+
+    return cov
+
+if __name__ == "__main__":
+    img = avgFace()
+    covid = getCov(img)
+    print(covid)
+
