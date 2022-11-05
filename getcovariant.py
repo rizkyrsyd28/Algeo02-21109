edf520fa0f77576e0496d0f5bb861adc9cf1f04f
Zaki <zakiamanullah215@gmail.com>
2022-11-05 23:10:09 +0700

    Ganti approach biar sama kaya spek

--- getcovariant.py
+++ getcovariant.py
@@ -3,23 +3,21 @@ import cv2
 from averageface import avgFace
 
 def getCov(avgimage):
-    res = np.empty(shape=(65536, 0), dtype=int)
+    res = np.zeros([256, 256], dtype=int)
 
     for i in range(1, 99):
         path = "../ALGEO02-21109/data/gray/CR" + str(i) + ".png";
         im = cv2.imread(path, 0)
         temp = np.subtract(im, avgimage)
-        temp = temp.reshape([65536, 1])
-        res = np.append(res, temp, axis=1)
+        transposed = np.transpose(temp)
+        cov = np.matmul(temp, transposed)
+        res = np.add(res, cov)
 
-    transposed = np.transpose(res)
-    cov = np.matmul(transposed, res)
-
-    return cov
+    res = np.multiply(1/98, res)
+    return res
 
 if __name__ == "__main__":
     img = avgFace()
     covid = getCov(img)
     print(covid)
     print(covid.shape)
-
