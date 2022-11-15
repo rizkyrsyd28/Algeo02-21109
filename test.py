65ed2cc231a2aed8b1000cd629cf67eabb747e8f
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2022-11-15 08:51:30 +0700

    file image

--- test.py
+++ test.py
@@ -1,26 +1,4 @@
-import cv2
-from numpy import linalg as al
-import numpy as np
+import os
 
-m = np.array([[3, -2, 4],[-2, 3, 1],[-1, 0, 5]], dtype=np.int8)
-
-# _,eigenval, eigenvec = cv2.eigen(m)
-
-# print(eigenval)
-# print(_)
-
-# V = al.eig(m)
-
-# print(V[0])
-# print(V[1])
-
-qr= al.qr(m)
-
-print("Q==============")
-print(qr[0])
-print("R==============")
-print(qr[1])
-print("M==============")
-X = np.matmul(qr[0],qr[1])
-X = X.astype(np.int8)
-print(X)
+for i in os.listdir("C://tugas besar//ALGEO-2//data//color"):
+    print(i)
