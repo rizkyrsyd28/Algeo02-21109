f0fc443fbb0811319951495c624c0e6e090c2a73
SaktiWidyatmaja <saktiwidyatmaja1@gmail.com>
2022-11-22 15:25:40 +0700

    Upload dataset baru

--- src/getcovariant.py
+++ src/getcovariant.py
@@ -1,40 +1,40 @@
 import numpy as np
 import cv2
-from averageface import getAvgFace, avgFace
+from averageface import getAvgFace
 import os
 import natsort
 
-def getCov(avgimage):
-    res = np.zeros([256, 256], dtype=int)
-    norm = np.empty([256, 256, 0], dtype=int)
+# def getCov(avgimage):
+#     res = np.zeros([256, 256], dtype=int)
+#     norm = np.empty([256, 256, 0], dtype=int)
 
-    for i in range(1, 99):
-        path = "../ALGEO02-21109/data/gray/CR" + str(i) + ".png";
-        im = cv2.imread(path, 0)
-        temp = np.subtract(im, avgimage)
-        norm = np.append(norm, np.atleast_3d(temp), axis=2)
-        transposed = np.transpose(temp)
-        cov = np.matmul(temp, transposed)
-        res = np.add(res, cov)
+#     for i in range(1, 99):
+#         path = "../ALGEO02-21109/data/gray/CR" + str(i) + ".png";
+#         im = cv2.imread(path, 0)
+#         temp = np.subtract(im, avgimage)
+#         norm = np.append(norm, np.atleast_3d(temp), axis=2)
+#         transposed = np.transpose(temp)
+#         cov = np.matmul(temp, transposed)
+#         res = np.add(res, cov)
 
-    res = np.multiply(1/98, res)
-    return (res, norm)
+#     res = np.multiply(1/98, res)
+#     return (res, norm)
 
-def getCovAlt(avgImg):
-    res = np.empty([65536, 0], dtype=int)
-    norm = np.empty([65536, 0], dtype=int)
+# def getCovAlt(avgImg):
+#     res = np.empty([65536, 0], dtype=int)
+#     norm = np.empty([65536, 0], dtype=int)
 
-    for i in range(1, 101):
-        path = "../ALGEO02-21109/data/gray/Test" + str(i) + ".png";
-        im = cv2.imread(path, 0)
-        temp = np.subtract(im, avgImg)
-        temp = np.reshape(temp, [65536, 1])
-        norm = np.append(norm, temp, axis=1)
-        res = np.append(res, temp, axis=1)
+#     for i in range(1, 101):
+#         path = "../ALGEO02-21109/data/gray/Test" + str(i) + ".png";
+#         im = cv2.imread(path, 0)
+#         temp = np.subtract(im, avgImg)
+#         temp = np.reshape(temp, [65536, 1])
+#         norm = np.append(norm, temp, axis=1)
+#         res = np.append(res, temp, axis=1)
 
-    transposed = np.transpose(res)
-    res = np.matmul(transposed, res)
-    return res, norm
+#     transposed = np.transpose(res)
+#     res = np.matmul(transposed, res)
+#     return res, norm
 
 def getCovariant(avgImg, dir):
     res = np.empty([65536, 0], dtype=int)
@@ -60,10 +60,10 @@ def getCovariant(avgImg, dir):
     return res, norm
 
 if __name__ == "__main__":
-    img = avgFace()
-    covid = getCov(img)
+    img = getAvgFace()
+    covid = getCovariant(img)
     print(covid)
     print(covid.shape)
-    alt = getCovAlt(img)
-    print(alt)
-    print(alt.shape)
+    # alt = getCovAlt(img)
+    # print(alt)
+    # print(alt.shape)
