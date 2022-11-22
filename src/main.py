7849f7d92888c1949d5e1e3fb3e5a234fa532dfc
rizkyrsyd28 <rizkyrasyid28@gmail.com>
2022-11-22 10:02:31 +0700

    hampir gan

--- src/main.py
+++ src/main.py
@@ -111,8 +111,8 @@ def predictImageIndex(testImage, trainingPath):
     testImg = cv2.cvtColor(testImage, cv2.COLOR_BGR2GRAY)
     testImg = cv2.resize(testImg, (256, 256))
     covariant, AMatrix = getcovariant.getCovariant(avgImg, trainingPath)
-    eigenValArrayPath = "../parahlimpik/eigen/eigenValue.txt"
-    adjustedEigenVecArrayPath = "../parahlimpik/eigen/eigenVec.txt"
+    eigenValArrayPath = "../ALGEO-2/eigen_parahlimpik/eigenValue.txt"
+    adjustedEigenVecArrayPath = "../ALGEO-2/eigen_parahlimpik/eigenVec.txt"
     startTime = time.time()
     if (exists(eigenValArrayPath) and exists(adjustedEigenVecArrayPath)):
         uVec = np.loadtxt(adjustedEigenVecArrayPath, dtype=float)
